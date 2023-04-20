4001ac720f118042d826a7fe3b2a0bfd50b68dc4
Happylang <lemarkmanlagnit8@gmail.com>
2023-04-20 10:18:16 +0800

    more updates on forget password

---
+++ app/src/main/java/com/example/konekq/activity_forget_password_successful_change_password.java
@@ -0,0 +1,14 @@
+package com.example.konekq;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_forget_password_successful_change_password extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password_successful_change_password);
+    }
+}
